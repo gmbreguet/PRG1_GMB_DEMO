3183aa1c37dad62fb0dc5ea0dd0c5464bb45cfde
gmbreguet <guy-michel.breguet@heig-vd.ch>
2024-09-30 17:36:53 +0200

    2024-09-30 17:36:53 - GMB

--- Complements/Cpp - IDE/entete_fonction.h
+++
@@ -1,8 +0,0 @@
-// nom         saisie
-// but         fonction de saisie
-//             la saisie est controllée
-// param       MSG_INVITE  message d'invite
-// param       MIN         intervalle de saisie, valeur min comprise
-// param       MAX         intervalle de saisie, valeur max comprise
-// return      valeur saisie
-// exception   n/a
